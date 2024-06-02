84d03f93e836f36a54c0b74f6a614971ce38aa9a
mg <mg>
2024-06-02 18:49:07 +0200

    update manifest, update strings, duplicate settingsactivity

---
+++ app/src/minSdk31/java/com/valerian/sharesong/SettingsActivity.kt
@@ -0,0 +1,128 @@
+package com.valerian.sharesong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.valerian.sharesong.ui.theme.ShareSongTheme
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class SettingsActivity : ComponentActivity() {
+    private val items = mutableMapOf(
+        ("Spotify" to "Spotify"),
+        ("Deezer" to "Deezer"),
+        ("Tidal" to "Tidal"),
+        ("AppleMusic" to "Apple Music"),
+        ("YoutubeMusic" to "Youtube Music")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val dataStore = (application as ShareSongApplication).dataStoreSingleton.getInstance()
+        val targetService = dataStore.data.map { it[SERVICE_OF_USER].orEmpty() }
+
+        setContent {
+            ShareSongTheme {
+                Surface(color = MaterialTheme.colorScheme.surface) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp, 48.dp)
+                            .fillMaxSize()
+                    ) {
+
+                        val selectedService = targetService.collectAsStateWithLifecycle(
+                            initialValue = "no"
+                        )
+                        val coroutineScope = rememberCoroutineScope()
+
+                        Text(
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp,
+                            text = "Choose your music service",
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        Text(
+                            fontSize = 16.sp,
+                            text = "If you click a link from a supported music service, it will be opened in ${items[selectedService.value]}.",
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            val itemKeys = items.keys.toList()
+                            itemsIndexed(itemKeys) { index, _ ->
+                                ButtonWithOutline(
+                                    text = items[itemKeys[index]].orEmpty(),
+                                    isSelected = itemKeys[index] == selectedService.value,
+                                    onSelected = {
+                                        coroutineScope.launch {
+                                            dataStore.edit { preferences ->
+                                                preferences[SERVICE_OF_USER] = itemKeys[index]
+                                            }
+                                        }
+                                    })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        val SERVICE_OF_USER = stringPreferencesKey("serviceOfUser")
+    }
+}
+
+@Composable
+fun ButtonWithOutline(
+    text: String, isSelected: Boolean, onSelected: () -> Unit
+) {
+    Button(
+        colors = if (isSelected) ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+        ) else ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        shape = RoundedCornerShape(16.dp),
+        onClick = onSelected,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(96.dp),
+        contentPadding = PaddingValues(8.dp),
+    ) {
+        Text(text = text, fontSize = 20.sp, textAlign = TextAlign.Center)
+    }
+}
